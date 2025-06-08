e20bc4af3f6bd8c31e176993e6dfe44f85b6025c
Aditi Singh <adithakurti@gmail.com>
2025-06-08 15:31:51 +0530

    added state management bloc to this

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/route_manager.dart';
-// import 'package:video/pages/home.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Video Conferencing',
-//       home: HomePage(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'blocs/meeting_bloc.dart';
--- lib/video_call.dart
+++ lib/video_call.dart
@@ -1,32 +1,3 @@
-// import 'package:agora_uikit/agora_uikit.dart';
-// import 'package:flutter/material.dart';
-
-// class VideoCall extends StatelessWidget {
-//   final AgoraClient _client = AgoraClient(
-//     agoraConnectionData: AgoraConnectionData(
-//       appId: "96c782160d8a483ea98ee09273cddb82",
-//       tempToken:
-//           "007eJxTYLjv9cXh0J6ToW+3PLHtuSzgvH3Tf0VHLYY37hy6DQb5sj0KDJZmyeYWRoZmBikWiSYWxqmJlhapqQaWRubGySkpSRZG+3v0MxoCGRkmBjkzMjJAIIjPwlCSWlzCwAAAXLMe6g==",
-//       channelName: "test",
-//     ),
-//     enabledPermission: [
-//       Permission.camera,
-//       Permission.microphone
-//     ]
-//   );
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(child: Stack(
-//         children: [
-//           AgoraVideoViewer(client: _client),
-//           AgoraVideoButtons(client: _client)
-//         ],
-//       )),
-//     );
-//   }
-// }
 import 'package:agora_uikit/agora_uikit.dart';
 import 'package:flutter/material.dart';
 
